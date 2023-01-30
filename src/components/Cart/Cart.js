7cac29be6a5d1bdf6938dcfffb62d4540547a475
Asel Williams <aselwilliams22@gmail.com>
2023-01-29 20:51:15 -0600

    adds checkout form

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,13 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
 import CartContext from '../../store/cart-context';
 import CartItem from './CartItem';
+import Checkout from './Checkout';
 
 const Cart = (props) => {
+    const [isCheckout, setIsCheckout] = useState(false)
+
     const cartCtx = useContext(CartContext);
 
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
@@ -17,6 +20,14 @@ const Cart = (props) => {
         cartCtx.addItem({...item, amount:1});
     }
 
+    const orderHandler = () => {
+        setIsCheckout(true)
+    }
+    const modalActions = <div className={classes.actions}>
+    <button className={classes['button--alt']} onClick={props.hideCartHandler}>Close</button>
+    {hasItems && <button className={classes.button} onClick={orderHandler}>Order</button>}
+</div>
+
     const cartItems = <ul className={classes['cart-items']}>{cartCtx.items.map((item)=> <CartItem key={item.id} name={item.name} amount={item.amount} price={item.price} onRemove={cartItemRemoveHandler.bind(null, item.id)} onAdd={cartItemAddHandler.bind(null, item)} />)}</ul>
   return (
     <Modal hideCartHandler={props.hideCartHandler}>
@@ -25,10 +36,8 @@ const Cart = (props) => {
             <span>Total Amount</span>
             <span>{totalAmount}</span>
         </div>
-        <div className={classes.actions}>
-            <button className={classes['button--alt']} onClick={props.hideCartHandler}>Close</button>
-            {hasItems && <button className={classes.button}>Order</button>}
-        </div>
+        {isCheckout && <Checkout onCancel={props.hideCartHandler} />}
+        {!isCheckout && modalActions}
     </Modal>
   )
 }
